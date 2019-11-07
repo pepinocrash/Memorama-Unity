3483d26c988d0faeb6c1cbaf7628202bc4d6e290
pepinocrash <45946088+pepinocrash@users.noreply.github.com>
2019-11-07 15:25:21 -0600

    Animaciones

--- Memorama_Unit/Assets/Scripts/Carta_pre.cs
+++ Memorama_Unit/Assets/Scripts/Carta_pre.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class Carta_pre : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Awake()
-    {
-        
-    }
+
+    public bool tarjeta_vol = false;
+
+    public static int tarjetas_volteadas;
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == transform.name /*&& tarjetas_volteadas>=2*/)
+                {
+                    tarjetas_volteadas = tarjetas_volteadas + 1;
+
+                    this.gameObject.GetComponent<Animator>().Play("cartar_voltear");
+                    this.gameObject.GetComponent<Animator>().StopPlayback();
+
+
+                }
+            }
+        }
+
     }
 }
