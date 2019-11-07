6f1b1883b96fd141d7fb70ae09b69c6fe456a017
pepinocrash <45946088+pepinocrash@users.noreply.github.com>
2019-11-06 18:07:08 -0600

    Tarjetitas

--- Memorama_Unit/Assets/Scripts/GameManager.cs
+++ Memorama_Unit/Assets/Scripts/GameManager.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public List<Texture> cartas;
+
+    public GameObject[] carta_scene;
+
+    int cantidad_maxima=4;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        carta_scene = GameObject.FindGameObjectsWithTag("carta");
+
+        for(int i=0; i<carta_scene.Length; i++)
+        {
+            int numero_list = Random.Range(0, cantidad_maxima);
+            Debug.Log(numero_list);
+
+            Material nuevo = carta_scene[i].transform.GetChild(0).GetComponent<Renderer>().material;
+
+            nuevo.mainTexture= cartas[numero_list];
+
+            cartas.Remove(cartas[numero_list]);
+
+            cantidad_maxima = cantidad_maxima - 1;
+
+            carta_scene[i].transform.GetChild(0).GetComponent<Renderer>().material = nuevo;
+        }
+
     }
 
     // Update is called once per frame
