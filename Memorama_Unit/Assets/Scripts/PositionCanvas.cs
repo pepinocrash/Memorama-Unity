fb18f12d1077001da4e674aa5e3214a11e379239
HexDrake <54444696+HexDrake@users.noreply.github.com>
2019-11-13 16:53:23 -0600

    Animation canvas

---
+++ Memorama_Unit/Assets/Scripts/PositionCanvas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionCanvas : MonoBehaviour
+{
+    public GameObject card01;
+    
+    public void renderCanvasImage01() {
+        card01.transform.SetSiblingIndex(1);
+    }
+    public void renderCanvasImage02()
+    {
+        card01.transform.SetSiblingIndex(2);
+    }
+
+}
