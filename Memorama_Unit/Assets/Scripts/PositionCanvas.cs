df03b03941d038064a50f4bef04d4b9160ff894f
pepinocrash <45946088+pepinocrash@users.noreply.github.com>
2019-11-14 20:36:39 -0600

    gui

--- Memorama_Unit/Assets/Scripts/PositionCanvas.cs
+++ Memorama_Unit/Assets/Scripts/PositionCanvas.cs
@@ -7,11 +7,11 @@ public class PositionCanvas : MonoBehaviour
     public GameObject card01;
     
     public void renderCanvasImage01() {
-        card01.transform.SetSiblingIndex(1);
+        card01.transform.SetSiblingIndex(0);
     }
     public void renderCanvasImage02()
     {
-        card01.transform.SetSiblingIndex(2);
+        card01.transform.SetSiblingIndex(1);
     }
 
 }
